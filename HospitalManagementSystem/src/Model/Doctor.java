0b469273e5136401f39d41cede4901b7d8037135
oykunsay <67629217+oykunsay@users.noreply.github.com>
2021-10-06 17:17:31 +0300

    Update Doctor.java

--- HospitalManagementSystem/src/Model/Doctor.java
+++ HospitalManagementSystem/src/Model/Doctor.java
@@ -1,65 +1,57 @@
-package Model;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 
-public class Doctor extends User {
-	
 	Statement st = null;
 	ResultSet rs = null;
 	Connection con = conn.connDb();
 	PreparedStatement preparedStatement = null;
-	
+
 	public Doctor() {
 		super();
 	}
-	
+
 	public Doctor(int id, String idno, String password, String name, String type) {
 		super(id, idno, password, name, type);
 	}
-	
+
 	public boolean addWhour(int doctor_id, String doctor_name, String wdate) throws SQLException {
 		int key = 0;
 		int count = 0;
 		String query = "INSERT INTO whour " + "(doctor_id, doctor_name, wdate) VALUES" + "(?,?,?)";
-		
+
 		try {
 			st = con.createStatement();
-			rs = st.executeQuery("SELECT * FROM whour WHERE status='a' AND doctor_id = " + doctor_id + " AND wdate ='" + wdate + "'");
-			
-			while(rs.next()) {
+			rs = st.executeQuery("SELECT * FROM whour WHERE status='a' AND doctor_id = " + doctor_id + " AND wdate ='"
+					+ wdate + "'");
+
+			while (rs.next()) {
 				count++;
 				break;
 			}
-			
-			if(count==0) {
+
+			if (count == 0) {
 				preparedStatement = con.prepareStatement(query);
 				preparedStatement.setInt(1, doctor_id);
 				preparedStatement.setString(2, doctor_name);
 				preparedStatement.setString(3, wdate);
 				preparedStatement.executeUpdate();
 			}
-			key=1;
+			key = 1;
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		if(key== 1)
+		if (key == 1)
 			return true;
-		else 
+		else
 			return false;
 	}
-	
-	public ArrayList<Whour> getWhourList(int doctor_id) throws SQLException{
+
+	public ArrayList<Whour> getWhourList(int doctor_id) throws SQLException {
 		ArrayList<Whour> list = new ArrayList<>();
 		Whour obj;
 		try {
 			st = con.createStatement();
 			rs = st.executeQuery("SELECT * FROM whour WHERE status ='a' AND doctor_id = " + doctor_id);
-			while(rs.next()) {
+			while (rs.next()) {
 				obj = new Whour();
 				obj.setId(rs.getInt("id"));
 				obj.setDoctor_id(rs.getInt("doctor_id"));
@@ -73,10 +65,9 @@ public class Doctor extends User {
 		}
 		return list;
 	}
-	
-	
+
 	public boolean deleteWhour(int id) throws SQLException {
-		
+
 		String query = "DELETE FROM whour WHERE id = ?";
 		boolean key = false;
 		try {
@@ -88,12 +79,11 @@ public class Doctor extends User {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		if(key)
+
+		if (key)
 			return true;
 		else
 			return false;
 	}
-	
-	
-}
+
+
