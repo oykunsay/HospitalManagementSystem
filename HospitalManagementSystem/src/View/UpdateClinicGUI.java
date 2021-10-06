d11329e0e2bf8260025e3eff8a88a8bc600b4957
oykunsay <67629217+oykunsay@users.noreply.github.com>
2021-10-06 18:13:55 +0300

    Update UpdateClinicGUI.java

--- HospitalManagementSystem/src/View/UpdateClinicGUI.java
+++ HospitalManagementSystem/src/View/UpdateClinicGUI.java
@@ -1,10 +1,14 @@
 package View;
+
 import java.awt.EventQueue;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
 import Helper.Helper;
 import Model.Clinic;
+
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTextField;
@@ -32,7 +36,6 @@ public class UpdateClinicGUI extends JFrame {
 		});
 	}
 
-	
 	public UpdateClinicGUI(final Clinic clinic) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 220, 150);
@@ -40,22 +43,22 @@ public class UpdateClinicGUI extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lbl_cName = new JLabel("Clinic Name");
 		lbl_cName.setFont(new Font("Serif", Font.PLAIN, 16));
 		lbl_cName.setBounds(10, 11, 185, 27);
 		contentPane.add(lbl_cName);
-		
+
 		fld_cName = new JTextField();
 		fld_cName.setColumns(10);
 		fld_cName.setBounds(10, 37, 185, 20);
 		fld_cName.setText(clinic.getName());
 		contentPane.add(fld_cName);
-		
+
 		JButton btn_cUPDATE = new JButton("UPDATE");
 		btn_cUPDATE.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(Helper.confirm("sure")) {
+				if (Helper.confirm("sure")) {
 					try {
 						clinic.updateClinic(clinic.getId(), fld_cName.getText());
 						Helper.showMsg("success");
