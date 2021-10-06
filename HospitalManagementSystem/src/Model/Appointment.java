795685d5faa5f5910cae46d43d53af9a846b2a5a
oykunsay <67629217+oykunsay@users.noreply.github.com>
2021-10-06 17:16:10 +0300

    Update Appointment.java

--- HospitalManagementSystem/src/Model/Appointment.java
+++ HospitalManagementSystem/src/Model/Appointment.java
@@ -7,132 +7,147 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-import Helper.DBConnection;
-
-public class Appointment {
-	private int id, doctorID, patientID;
-	private String doctorName, patientName, appDate;
-
-	DBConnection conn = new DBConnection();
+public class ChiefPhysician extends User {
+	Connection con = conn.connDb();
 	Statement st = null;
 	ResultSet rs = null;
 	PreparedStatement preparedStatement = null;
 
-	public Appointment(int id, int doctorID, int patientID, String doctorName, String patientName, String appDate) {
-		super();
-		this.id = id;
-		this.doctorID = doctorID;
-		this.patientID = patientID;
-		this.doctorName = doctorName;
-		this.patientName = patientName;
-		this.appDate = appDate;
+	public ChiefPhysician(int id, String idno, String password, String name, String type) {
+		super(id, idno, password, name, type);
 	}
 
-	public Appointment() {
+	public ChiefPhysician() {
 	}
 
-	public ArrayList<Appointment> getPatientList(int patient_id) throws SQLException {
-		ArrayList<Appointment> list = new ArrayList<>();
-		Appointment obj;
-		Connection con = conn.connDb();
+	public ArrayList<User> getDoctorList() throws SQLException {
+		ArrayList<User> list = new ArrayList<>();
+		User obj;
 		try {
 			st = con.createStatement();
-			rs = st.executeQuery("SELECT * FROM appointment WHERE patient_id = " + patient_id);
+			rs = st.executeQuery("SELECT * FROM user WHERE type = 'doctor'");
 			while (rs.next()) {
-				obj = new Appointment();
-				obj.setId(rs.getInt("id"));
-				obj.setDoctorID(rs.getInt("doctor_id"));
-				obj.setDoctorName(rs.getString("doctor_name"));
-				obj.setPatientID(rs.getInt("patient_id"));
-				obj.setPatientName(rs.getString("patient_name"));
-				obj.setAppDate(rs.getString("app_date"));
+				obj = new User(rs.getInt("id"), rs.getString("idno"), rs.getString("password"), rs.getString("name"),
+						rs.getString("type"));
 				list.add(obj);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} finally {
-			st.close();
-			rs.close();
-			con.close();
 		}
-
 		return list;
 	}
 
-	public ArrayList<Appointment> getDoctorList(int doctor_id) throws SQLException {
-		ArrayList<Appointment> list = new ArrayList<>();
-		Appointment obj;
-		Connection con = conn.connDb();
+	public ArrayList<User> getClinicDoctorList(int clinicID) throws SQLException {
+		ArrayList<User> list = new ArrayList<>();
+		User obj;
 		try {
 			st = con.createStatement();
-			rs = st.executeQuery("SELECT * FROM appointment WHERE docotor_id = " + doctor_id);
+			rs = st.executeQuery(
+					"SELECT u.id,u.idno,u.password,u.name,u.type FROM worker w LEFT JOIN user u ON w.user_id = u.id WHERE clinic_id = "
+							+ clinic_id);
 			while (rs.next()) {
-				obj = new Appointment();
-				obj.setId(rs.getInt("id"));
-				obj.setDoctorID(rs.getInt("doctor_id"));
-				obj.setDoctorName(rs.getString("doctor_name"));
-				obj.setPatientID(rs.getInt("patient_id"));
-				obj.setPatientName(rs.getString("patient_name"));
-				obj.setAppDate(rs.getString("app_date"));
+				obj = new User(rs.getInt("u.id"), rs.getString("u.idno"), rs.getString("u.password"),
+						rs.getString("u.name"), rs.getString("u.type"));
 				list.add(obj);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} finally {
-			st.close();
-			rs.close();
-			con.close();
 		}
-
 		return list;
 	}
 
-	public int getId() {
-		return id;
+	public boolean addDoctor(String idno, String password, String name) throws SQLException {
+
+		String query = "INSERT INTO user" + "(idno, password, name, type) VALUES" + "(?,?,?,?)";
+		boolean key = false;
+		try {
+			st = con.createStatement();
+			preparedStatement = con.prepareStatement(query);
+			preparedStatement.setString(1, idno);
+			preparedStatement.setString(2, password);
+			preparedStatement.setString(3, name);
+			preparedStatement.setString(4, "doctor");
+			preparedStatement.executeUpdate();
+			key = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		if (key)
+			return true;
+		else
+			return false;
 	}
 
-	public void setId(int id) {
-		this.id = id;
+	public boolean deleteDoctor(int id) throws SQLException {
+
+		String query = "DELETE FROM user WHERE id = ?";
+		boolean key = false;
+		try {
+			st = con.createStatement();
+			preparedStatement = con.prepareStatement(query);
+			preparedStatement.setInt(1, id);
+			preparedStatement.executeUpdate();
+			key = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		if (key)
+			return true;
+		else
+			return false;
 	}
 
-	public int getDoctorID() {
-		return doctorID;
+	public boolean updateDoctor(int id, String idno, String password, String name) throws SQLException {
+
+		String query = "UPDATE user SET name = ?, idno = ?, password = ? WHERE id = ?";
+		boolean key = false;
+		try {
+			st = con.createStatement();
+			preparedStatement = con.prepareStatement(query);
+			preparedStatement.setString(1, name);
+			preparedStatement.setString(2, idno);
+			preparedStatement.setString(3, password);
+			preparedStatement.setInt(4, id);
+			preparedStatement.executeUpdate();
+			key = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		if (key)
+			return true;
+		else
+			return false;
 	}
 
-	public void setDoctorID(int doctorID) {
-		this.doctorID = doctorID;
-	}
+	public boolean addWorker(int userID, int clinicID) throws SQLException {
 
-	public int getPatientID() {
-		return patientID;
-	}
+		String query = "INSERT INTO worker" + "(user_id, clinic_id) VALUES" + "(?,?)";
+		boolean key = false;
+		int count = 0;
+		try {
+			st = con.createStatement();
+			rs = st.executeQuery("SELECT * FROM worker WHERE clinic_id=" + clinicID + " AND user_id=" + userID);
+			while (rs.next()) {
+				count++;
+			}
+			if (count == 0) {
+				preparedStatement = con.prepareStatement(query);
+				preparedStatement.setInt(1, userID);
+				preparedStatement.setInt(2, clinicID);
+				preparedStatement.executeUpdate();
+			}
+			key = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-	public void setPatientID(int patientID) {
-		this.patientID = patientID;
-	}
-
-	public String getDoctorName() {
-		return doctorName;
-	}
-
-	public void setDoctorName(String doctorName) {
-		this.doctorName = doctorName;
-	}
-
-	public String getPatientName() {
-		return patientName;
-	}
-
-	public void setPatientName(String patientName) {
-		this.patientName = patientName;
-	}
-
-	public String getAppDate() {
-		return appDate;
-	}
-
-	public void setAppDate(String appDate) {
-		this.appDate = appDate;
+		if (key)
+			return true;
+		else
+			return false;
 	}
 
 }
