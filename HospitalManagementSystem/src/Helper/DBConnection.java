875b1eaa1f6517d59b71535bf3b73ff5bafe8d0d
oykunsay <67629217+oykunsay@users.noreply.github.com>
2021-10-06 17:01:27 +0300

    Update DBConnection.java

--- HospitalManagementSystem/src/Helper/DBConnection.java
+++ HospitalManagementSystem/src/Helper/DBConnection.java
@@ -1,11 +1,13 @@
 package Helper;
+
 import java.sql.*;
 
 public class DBConnection {
 	Connection c = null;
-	
-	public DBConnection() {}
-	
+
+	public DBConnection() {
+	}
+
 	public Connection connDb() {
 		try {
 			this.c = DriverManager.getConnection("jdbc:mariadb://localhost:3306/hospital?user=root&password=1234");
@@ -16,3 +18,4 @@ public class DBConnection {
 		return c;
 	}
 }
+
