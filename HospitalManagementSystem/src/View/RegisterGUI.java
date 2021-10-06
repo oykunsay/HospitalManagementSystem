4ddc06d9aab3e87d5e8d0b1ae44b51c025868b6d
oykunsay <67629217+oykunsay@users.noreply.github.com>
2021-10-06 18:15:12 +0300

    Update RegisterGUI.java

--- HospitalManagementSystem/src/View/RegisterGUI.java
+++ HospitalManagementSystem/src/View/RegisterGUI.java
@@ -1,8 +1,11 @@
 package View;
+
 import java.awt.EventQueue;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
 import Helper.Helper;
 import Model.Patient;
 import java.awt.Color;
@@ -24,7 +27,7 @@ public class RegisterGUI extends JFrame {
 	private Patient patient = new Patient();
 
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable(){
+		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					RegisterGUI frame = new RegisterGUI();
@@ -36,7 +39,6 @@ public class RegisterGUI extends JFrame {
 		});
 	}
 
-	
 	public RegisterGUI() {
 		setResizable(false);
 		setTitle("Hospital Management System");
@@ -47,50 +49,51 @@ public class RegisterGUI extends JFrame {
 		w_pane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(w_pane);
 		w_pane.setLayout(null);
-		
+
 		JLabel label = new JLabel("Name Surname");
 		label.setFont(new Font("Serif", Font.PLAIN, 16));
 		label.setBounds(10, 11, 135, 27);
 		w_pane.add(label);
-		
+
 		fld_name = new JTextField();
 		fld_name.setColumns(10);
 		fld_name.setBounds(10, 37, 221, 20);
 		w_pane.add(fld_name);
-		
+
 		JLabel label_1 = new JLabel("Identity Number");
 		label_1.setFont(new Font("Serif", Font.PLAIN, 16));
 		label_1.setBounds(10, 68, 135, 27);
 		w_pane.add(label_1);
-		
+
 		fld_idno = new JTextField();
 		fld_idno.setColumns(10);
 		fld_idno.setBounds(10, 95, 221, 20);
 		w_pane.add(fld_idno);
-		
+
 		JLabel label_2 = new JLabel("Password");
 		label_2.setFont(new Font("Serif", Font.PLAIN, 16));
 		label_2.setBounds(10, 126, 134, 27);
 		w_pane.add(label_2);
-		
+
 		fld_pass = new JPasswordField();
 		fld_pass.setBounds(10, 153, 221, 20);
 		w_pane.add(fld_pass);
-		
+
 		JButton btn_register = new JButton("SIGN UP");
 		btn_register.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(fld_idno.getText().length() == 0 || fld_pass.getText().length() == 0 || fld_name.getText().length() == 0) {
+				if (fld_idno.getText().length() == 0 || fld_pass.getText().length() == 0
+						|| fld_name.getText().length() == 0) {
 					Helper.showMsg("fill");
-				}else {
+				} else {
 					try {
-						boolean control = patient.register(fld_idno.getText(), fld_pass.getText() , fld_name.getText());
-						if(control) {
+						boolean control = patient.register(fld_idno.getText(), fld_pass.getText(), fld_name.getText());
+						if (control) {
 							Helper.showMsg("success");
 							LoginGUI login = new LoginGUI();
 							login.setVisible(true);
 							dispose();
-						}else {
+						} else {
 							Helper.showMsg("error");
 						}
 					} catch (SQLException e1) {
@@ -102,7 +105,7 @@ public class RegisterGUI extends JFrame {
 		btn_register.setFont(new Font("Serif", Font.PLAIN, 13));
 		btn_register.setBounds(10, 184, 221, 23);
 		w_pane.add(btn_register);
-		
+
 		JButton btn_backto = new JButton("CANCEL");
 		btn_backto.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
